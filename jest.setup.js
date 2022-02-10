93af884b7fb9ae1df1722edfcd69e453111e7afc
Dean Scarff <dos@scarff.id.au>
2022-02-11 04:07:12 +1100

    Add unit test for DiscordClient
    Add jest frameworks for testing Chrome and typescript.

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+// This script is run by jest (setupFilesAfterEnv) before tests.
+
+// Mock implementations of the Chrome API.
+Object.assign(global, require('jest-chrome'));
+
+// Suppress debug logging.
+global.console.debug = () => {};
---
+++ src/background/DiscordClient.test.ts
@@ -0,0 +1,35 @@
+import { mockDeep } from 'jest-mock-extended';
+import { chrome } from 'jest-chrome';
+import DiscordClient from './DiscordClient';
+
+describe('DiscordClient', () => {
+  const mockPort = mockDeep<chrome.runtime.Port>();
+  chrome.runtime.connectNative.mockReturnValue(mockPort);
+  const client = new DiscordClient('TestClient');
+
+  it('connect creates port', () => {
+    expect(chrome.runtime.connectNative)
+      .toHaveBeenCalledWith('io.github.p00ya.cdb');
+  });
+
+  it('sendHandshake posts message', () => {
+    client.sendHandshake();
+    expect(mockPort.postMessage).toHaveBeenCalledTimes(1);
+    const [[handshake]] = mockPort.postMessage.mock.calls;
+    expect(handshake.client_id).toBe('TestClient');
+  });
+
+  it('sendActivity posts message', () => {
+    mockPort.postMessage.mockReset();
+    client.sendActivity('TestActivity');
+    expect(mockPort.postMessage).toHaveBeenCalledTimes(1);
+    const [[activityFrame]] = mockPort.postMessage.mock.calls;
+    expect(activityFrame.args.activity.state).toBe('TestActivity');
+    expect(activityFrame.cmd).toBe('SET_ACTIVITY');
+  });
+
+  it('disconnect disconnects port', () => {
+    client.disconnect();
+    expect(mockPort.disconnect).toHaveBeenCalledTimes(1);
+  });
+});
