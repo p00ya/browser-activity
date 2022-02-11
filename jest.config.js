a064a19a40144ef510c2cf7346e9032cdd7f05d6
Dean Scarff <dos@scarff.id.au>
2022-02-11 15:21:47 +1100

    Increase slowTestThreshold from 5s to 10s

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jest-environment-jsdom-global',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  slowTestThreshold: 10,  // Even just loading modules is ~3s.
 };
