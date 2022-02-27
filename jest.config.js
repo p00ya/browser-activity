f32c9370563324a2b8043ff60d9bb011bdf81dc9
Dean Scarff <dos@scarff.id.au>
2022-02-27 22:21:41 +1100

    Silence TypeScript warning
    Silence TS151001 suggestions to use --esModuleInterop; as far as I can
    tell it's not actually needed.

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,17 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   slowTestThreshold: 10, // Even just loading modules is ~3s.
   maxWorkers: 1, // Still faster than default with 2 physical cores.
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      diagnostics: {
+        ignoreCodes: [
+          6059, // ts-jest default
+          18002, // ts-jest default
+          18003, // ts-jest default
+          151001, // "consider setting `esModuleInterop` to `true`"
+        ],
+      },
+    },
+  },
 };
